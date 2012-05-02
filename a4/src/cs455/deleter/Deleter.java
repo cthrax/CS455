c786a5b9731184d58ee482811c00ae57802851fe
Jeffrey Turley <jturley@rams.colostate.edu>
2012-05-02 17:26:18 -0600

    breaking things

--- a4/src/cs455/deleter/Deleter.java
+++ a4/src/cs455/deleter/Deleter.java
@@ -1,5 +1,7 @@
 package cs455.deleter;
 
+import cs455.statistics.*;
+
 import java.util.StringTokenizer;
 import java.io.IOException;
 
@@ -8,23 +10,24 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.join.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
 
 public class Deleter {
 
-    public class DeleteMapper extends Mapper<Text, TupleWritable, Text, TupleWritable> {
+    public class DeleteMapper extends Mapper<Text, TupleWritable, Text, Text> {
+        private final String sentence = "Twas a dark kayak and stormy night";
         private IntWritable count;
         private Text second;
         private Text first;
 
         public void map(Text key, TupleWritable value, Context context) throws IOException, InterruptedException {
-            //TODO: Implement DeleteMapper
-//            String line = value.toString();
-//            StringTokenizer tok = new StringTokenizer(line);
-//
-//            while(tok.hasMoreTokens()) {
-//                first.set(tok.nextToken());
-//                context.write(word, count);
-//            }
+            String line = value.toString();
+            StringTokenizer tok = new StringTokenizer(line);
+
+            while(tok.hasMoreTokens()) {
+                first.set(tok.nextToken());
+            }
+            
         }
     }
 
@@ -36,28 +39,22 @@ public class Deleter {
     }
 
     public static void main(String[] args) throws Exception {
-        String input = args[0];
-        for(int i = 1; i < args.length; i++) {
-            input += " " + args[i];
-        }
-        Text sentence = new Text(input);
+
         // Create a new job
         Configuration jobConf = new Configuration();
 
-        jobConf.set("sentence", input);
-        jobConf.addResource(new Path("/probabilites/foo"));
-
         Job deletion = Job.getInstance(jobConf);
-        deletion.setInputFormatClass(CompositeInputFormat.class);
-//        deletion.setOuptutFormat(); //TODO: These two lines
-//        deletion.setOutputValueFormat();
+        deletion.setInputFormatClass(KeyValueTextInputFormat.class);
+        deletion.setOuptutFormat(TextOutputFormat.class);
         deletion.setJarByClass(Deleter.class);
 
-        // Specify job-specific parameters
         deletion.setJobName("Deleter");
 
         deletion.setMapperClass(DeleteMapper.class);
         deletion.setReducerClass(DeleteReducer.class);
+
+        FileInputFormat.addInputPath(deletion, new Path(args[0]);
+        FileOutputFormat.addOutputPath(deletion, new Path(args[1]);
 
         // Submit the job, poll for progress until it completes
         deletion.waitForCompletion(true);
