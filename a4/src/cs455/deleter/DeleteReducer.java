df087a4baea490bc5e9b798b201fb14fc79887c4
Kyle Rudd <rudd@rutabaga.cs.colostate.edu>
2012-05-02 21:57:36 -0600

    The reducer should now finally be working

--- a4/src/cs455/deleter/DeleteReducer.java
+++ a4/src/cs455/deleter/DeleteReducer.java
@@ -15,10 +15,19 @@ import org.apache.hadoop.mapreduce.lib.input.*;
 public class DeleteReducer extends Reducer<Text, Text, Text, Text> {
 
    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+	LinkedList<Text> hackIter = new LinkedList<Text>();
       int kickerIndex = -1;
       float kickerProb = Float.MAX_VALUE;
+/*	String garbage = "notheing";
+	for (Text val : values) {
+	    String[] split =  val.toString().split(" ");
+	    garbage = split[2];
+	    context.write(val, new Text(garbage));
+	}
+*/
       int i = 0;
       for (Text val : values) {
+	 hackIter.add(new Text(val.toString()));
          String[] split = val.toString().split(" ");
          float prob = Float.parseFloat(split[2]);
          if (prob < kickerProb) {
@@ -28,11 +37,10 @@ public class DeleteReducer extends Reducer<Text, Text, Text, Text> {
          i++;
       }
       i = 0;
-      for (Text val : values) {
+      for (Text val : hackIter) {
          String[] split = val.toString().split(" ");
          if (i != kickerIndex) {
-            String out = split[0];
-            out += split[1];
+            String out = split[0] + " " + split[1];
             context.write(key, new Text(out));
          }
          i++;
