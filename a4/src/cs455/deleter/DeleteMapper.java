decb5070d1f94db347fd83bdeeb8e2a781ca6624
Myles Bostwick <myles@schneider-electric.com>
2012-05-03 13:27:30 -0600

    Cleanup mapper code.

--- a4/src/cs455/deleter/DeleteMapper.java
+++ a4/src/cs455/deleter/DeleteMapper.java
@@ -9,47 +9,54 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.join.*; 
+import org.apache.hadoop.mapreduce.lib.join.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 
 public class DeleteMapper extends Mapper<LongWritable, Text, Text, Text> {
-   private static String sentence;
-   private static HashMap<String, LinkedList<String>> sentenceBigrams;
-   private IntWritable count;
-   private Text second;
-   private Text first;
+	private static String sentence;
+	private static HashMap<String, LinkedList<String>> sentenceBigrams;
+	private IntWritable count;
+	private Text second;
+	private Text first;
 
-   public static void setSentence(String input) {
-      sentenceBigrams = new HashMap<String, LinkedList<String>>();
-      sentence = input;
-      String[] split = input.split(" ");
-      for (int i = 0; i < split.length-1; i++) {
-         System.out.println("Bigram "+i+": "+split[i]+" "+split[i+1]);
-         String first = split[i];
-         String second = split[i+1];
-         if (!sentenceBigrams.containsKey(second)) {
-            sentenceBigrams.put(second, new LinkedList<String>());
-         }
-         sentenceBigrams.get(second).add(first);
-      }
-   }
+	public static void setSentence(String input) {
+		sentenceBigrams = new HashMap<String, LinkedList<String>>();
+		sentence = input;
+		String[] split = input.split(" ");
+		for (int i = 0; i < split.length - 1; i++) {
+			System.out.println("Bigram " + i + ": " + split[i] + " "
+					+ split[i + 1]);
+			String first = split[i];
+			String second = split[i + 1];
+			if (!sentenceBigrams.containsKey(second)) {
+				sentenceBigrams.put(second, new LinkedList<String>());
+			}
+			sentenceBigrams.get(second).add(first);
+		}
+	}
 
-   public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-      //      second = key;
-      String[] line = value.toString().split("\t");
-      second = new Text(line[0]);
-      line = value.toString().substring(value.toString().indexOf("\t")).split(";");
+	public void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		// second = key;
+		String[] line = value.toString().split("\t");
+		second = new Text(line[0]);
+		line = line[1].split(";");
 
-      for(String s : line) {
-         first = new Text(s.split(",")[0]);
-         Float prob = Float.parseFloat(s.split(",")[1]);
-         if (sentenceBigrams.containsKey(second.toString())) {
-            if(sentenceBigrams.get(second.toString()).contains(first.toString())) {
-               System.out.println("FOUND: "+first+" "+second);
-               context.write(new Text(sentence), new Text(first.toString() + " " + second.toString() + " " + prob));
-            } else { 
-            //   context.write(new Text(sentence), new Text(first.toString() + " " + second.toString() + " " + 0f));
-            }}
-         }
-      }
-   }
+		for (String s : line) {
+			String[] bigram = s.split(",");
+			first = new Text(bigram[0]);
+			Float prob = Float.parseFloat(bigram[1]);
+			if (sentenceBigrams.containsKey(second.toString())) {
+				if (sentenceBigrams.get(second.toString()).contains(
+						first.toString())) {
+					System.out.println("FOUND: " + first + " " + second);
+					context.write(new Text(sentence), new Text(first.toString()
+							+ " " + second.toString() + " " + prob));
+				} else {
+					context.write(new Text(sentence), new Text(first.toString()
+							+ " " + second.toString() + " " + 0f));
+				}
+			}
+		}
+	}
+}
