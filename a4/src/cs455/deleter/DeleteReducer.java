3cddae28778725ce4bd32310ea8b0579ed6a91b0
Kyle Rudd <rudd@parsley.cs.colostate.edu>
2012-05-03 13:25:36 -0600

    Fixed Delete reducer

--- a4/src/cs455/deleter/DeleteReducer.java
+++ a4/src/cs455/deleter/DeleteReducer.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 import java.util.HashMap;
 import java.io.IOException;
+//import java.util.StringBuilder
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -16,35 +17,41 @@ public class DeleteReducer extends Reducer<Text, Text, Text, Text> {
 
    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 	LinkedList<Text> hackIter = new LinkedList<Text>();
-      int kickerIndex = -1;
-      float kickerProb = Float.MAX_VALUE;
-/*	String garbage = "notheing";
-	for (Text val : values) {
-	    String[] split =  val.toString().split(" ");
-	    garbage = split[2];
-	    context.write(val, new Text(garbage));
+	int kickerIndex = -1;
+	float kickerProb = Float.MAX_VALUE;
+	int i = 0;
+     	for (Text val : values) {
+	    hackIter.add(new Text(val.toString()));
+            String[] split = val.toString().split(" ");
+            float prob = Float.parseFloat(split[2]);
+            if (prob < kickerProb) {
+            	kickerIndex = i;
+            	kickerProb = prob; 
+            }
+            i++;
+      	}
+      	String[] sentence = key.toString().split(" ");;
+	String toRemove = hackIter.get(kickerIndex).toString().split(" ")[0];
+	StringBuilder out = new StringBuilder();
+	for (String s : sentence) {
+	    if (!s.equals(toRemove)) {
+		out.append(s);
+		out.append(" ");
+	    }
 	}
-*/
-      int i = 0;
-      for (Text val : values) {
-	 hackIter.add(new Text(val.toString()));
-         String[] split = val.toString().split(" ");
-         float prob = Float.parseFloat(split[2]);
-         if (prob < kickerProb) {
-            kickerIndex = i;
-            kickerProb = prob; 
-         }
-         i++;
-      }
+	context.write(new Text(out.toString()), new Text(""));
+      /*
       i = 0;
+      StringBuilder builder = new StringBuilder();
       for (Text val : hackIter) {
          String[] split = val.toString().split(" ");
          if (i != kickerIndex) {
-            String out = split[0] + " " + split[1];
-            context.write(key, new Text(out));
+            builder.append(split[0] + " ");
          }
          i++;
       }
+      context.write(key, new Text(builder.toString()));
+      */
    }
 }
 
